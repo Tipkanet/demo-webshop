3ed3b300926f9f451e0777b0f0c98499b140fed6
tipka <tipkanet@gmail.com>
2021-11-20 19:46:31 +0200

    add successfulLoginTest

--- src/test/java/WebShopTests.java
+++ src/test/java/WebShopTests.java
@@ -3,55 +3,39 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.is;
 
 public class WebShopTests {
-
     @Test
-    @DisplayName("Successfully adding the fist item to a new cart")
-    void addFirstItemToCardTest() {
-        Response response =
-        given()
-                .contentType("application/x-www-form-urlencoded; charset=UTF-8")
-                .body("product_attribute_72_5_18=53&product_attribute_72_6_19=54&" +
-                        "product_attribute_72_3_20=57&addtocart_72.EnteredQuantity=1")
-                .when()
-                .post("http://demowebshop.tricentis.com/addproducttocart/details/72/1")
-                .then()
-                .statusCode(200)
-                .body("success", is(true))
-                .body("message", is("The product has been added to your <a href=\"/cart\">shopping cart</a>"))
-                .body("updatetopcartsectionhtml", is("(1)"))
-                .extract().response();
-        System.out.println(response.asString());
-        System.out.println(response.path("updatetopcartsectionhtml").toString());
-    }
-
-    @Test
-    @DisplayName("Successfully adding the items to the cart, with Cookie")
-    void addItemsToCardTest() {
+    @DisplayName("Successful authorization: should return status code 302")
+    void successfulLoginTest() {
         Response response =
                 given()
-                        .contentType("application/x-www-form-urlencoded; charset=UTF-8")
-                        .body("product_attribute_72_5_18=53&product_attribute_72_6_19=54&" +
-                                "product_attribute_72_3_20=57&addtocart_72.EnteredQuantity=1")
-                        .cookie("Nop.customer=af338eec-9318-487c-8632-abaa053eb9ee; " +
-                                "NopCommerce.RecentlyViewedProducts=RecentlyViewedProductIds=72; " +
-                                "__utmz=78382081.1637072055.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); " +
-                                "ARRAffinity=7f10010dd6b12d83d6aefe199065b2e8fe0d0850a7df2983b482815225e42439; " +
-                                "__utmc=78382081; " +
-                                "__utma=78382081.1119391476.1637072055.1637162878.1637165106.5; " +
-                                "__atuvc=5%7C46; __atuvs=61952832de76133b001; __utmb=78382081.2.10.1637165106")
+                        .contentType("application/x-www-form-urlencoded")
+                        .body("Email=qa%40qaqa.com&Password=123456&RememberMe=false")
+                        .cookie("Nop.customer=4982d5ec-b850-4aef-ba2e-73abbeeb18e5")
                         .when()
-                        .post("http://demowebshop.tricentis.com/addproducttocart/details/72/1")
+                        .post("http://demowebshop.tricentis.com/login/")
                         .then()
-                        .statusCode(200)
-                        .body("success", is(true))
-                        .body("message", is("The product has been added to your <a href=\"/cart\">shopping cart</a>"))
-//                        .body("updatetopcartsectionhtml", is("(1)")) increase +1
+                        .statusCode(302)
                         .extract().response();
-        System.out.println(response.asString());
-        System.out.println(response.path("updatetopcartsectionhtml").toString());
+        assertThat(response.contentType()).isEqualTo("text/html; charset=utf-8");
     }
 
+    @Test
+    @DisplayName("Successful authorization: should return status code 302")
+    void successfulLoginWithNonEmptyCartTest() {
+        Response response =
+                given()
+                        .contentType("application/x-www-form-urlencoded")
+                        .body("Email=qa%40qaqa.com&Password=123456&RememberMe=false")
+                        .cookie("Nop.customer=4982d5ec-b850-4aef-ba2e-73abbeeb18e5")
+                        .when()
+                        .post("http://demowebshop.tricentis.com/login/")
+                        .then()
+                        .statusCode(302)
+                        .extract().response();
+        assertThat(response.contentType()).isEqualTo("text/html; charset=utf-8");
+    }
 }
