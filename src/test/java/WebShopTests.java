6a86fd59eb9514a05af30af0e4b234b437ca451e
tipka <tipkanet@gmail.com>
2021-11-26 11:34:23 +0200

    add successful login

--- src/test/java/WebShopTests.java
+++ src/test/java/WebShopTests.java
@@ -1,41 +1,30 @@
-import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Cookie;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static io.restassured.RestAssured.given;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.Matchers.is;
 
 public class WebShopTests {
     @Test
-    @DisplayName("Successful authorization: should return status code 302")
+    @DisplayName("Successful authorization")
     void successfulLoginTest() {
-        Response response =
+        String loginCookie =
                 given()
                         .contentType("application/x-www-form-urlencoded")
-                        .body("Email=qa%40qaqa.com&Password=123456&RememberMe=false")
-                        .cookie("Nop.customer=4982d5ec-b850-4aef-ba2e-73abbeeb18e5")
+                        .formParam("Email", "sigwist@qa.com")
+                        .formParam("Password", "#Pojo9")
                         .when()
                         .post("http://demowebshop.tricentis.com/login/")
                         .then()
                         .statusCode(302)
-                        .extract().response();
-        assertThat(response.contentType()).isEqualTo("text/html; charset=utf-8");
-    }
-
-    @Test
-    @DisplayName("Successful authorization: should return status code 302")
-    void successfulLoginWithNonEmptyCartTest() {
-        Response response =
-                given()
-                        .contentType("application/x-www-form-urlencoded")
-                        .body("Email=qa%40qaqa.com&Password=123456&RememberMe=false")
-                        .cookie("Nop.customer=4982d5ec-b850-4aef-ba2e-73abbeeb18e5")
-                        .when()
-                        .post("http://demowebshop.tricentis.com/login/")
-                        .then()
-                        .statusCode(302)
-                        .extract().response();
-        assertThat(response.contentType()).isEqualTo("text/html; charset=utf-8");
+                        .extract().cookie("NOPCOMMERCE.AUTH");
+        open("http://demowebshop.tricentis.com/Themes/DefaultClean/Content/images/logo.png");
+        getWebDriver().manage().addCookie(new Cookie("NOPCOMMERCE.AUTH", loginCookie));
+        open("http://demowebshop.tricentis.com");
+        $(".account").shouldHave(text("sigwist@qa.com"));
     }
 }
